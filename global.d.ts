0e16a8abf079a692f6c54bfd6ce3bb23a5b072fc
iamshivamkr <shivam__kumar@outlook.com>
2024-01-07 00:11:25 +0530

    Final

---
+++ global.d.ts
@@ -0,0 +1,5 @@
+declare module "use-sound" {
+  export default function useSound(sound: any, options?: any): any;
+}
+
+// error handle by this of Use-Sound in PlayerContent
---
+++ providers/ModalProvider.tsx
@@ -0,0 +1,31 @@
+"use client";
+import AuthModal from "@/components/AuthModal";
+import SubscribeModal from "@/components/SubscribeModal";
+import UploadModal from "@/components/UploadModal";
+import { ProductWithPrice } from "@/types";
+
+import { useState, useEffect } from "react";
+
+interface ModalProviderProps {
+  products: ProductWithPrice[];
+}
+
+const ModalProvider: React.FC<ModalProviderProps> = ({ products }) => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+  if (!isMounted) {
+    return null;
+  }
+  return (
+    <>
+      <AuthModal />
+      <SubscribeModal products={products} />
+      <UploadModal />
+    </>
+  );
+};
+
+export default ModalProvider;
